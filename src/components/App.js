c71f685094a5392dd21413ec0dbb1971c9287b10
Vrushali Murlidhar Patil <vrushalimpatil2001@gamil.com>
2023-05-11 20:57:42 +0530

    submitting code-Thu May 11 2023 20:57:40 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [name, setName] = useState("");
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <form>
+          <label>Enter your name:</label>
+          <br></br>
+          <input type="text" onChange={(e) =>{setName(e.target.value)}}/>
+        </form>
+        <p>{name ? `Hello ${name}!` : ""}</p>
     </div>
   )
 }
